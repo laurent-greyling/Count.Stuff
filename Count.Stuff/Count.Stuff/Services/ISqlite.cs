25c31fa0a586390a082137f2d5271d9d8a6c1117
laurent.greyling@gmail.com <laurent.greyling@gmail.com>
2018-08-15 13:53:45 +0200

    Remove UWP as not going to concentrate on making all work here as well

--- Count.Stuff/Count.Stuff/Services/ISqlite.cs
+++ Count.Stuff/Count.Stuff/Services/ISqlite.cs
@@ -4,7 +4,6 @@ namespace Count.Stuff.Services
 {
     /// <summary>
     /// For Android <see cref="Droid.Services"/>
-    /// For Uwp <see cref="UWP.Services"/>
     /// </summary>
     interface ISqlite
     {
